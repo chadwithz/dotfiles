309c5d5f2da778a7898dd136a29fd337ee707a20
Ihsan Kurniawan <55323174+ihsankl@users.noreply.github.com>
2022-05-23 15:51:10 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ echo "===================update & upgrading systems==================="
 sudo apt-get update && sudo apt-get upgrade -y
 
 echo "===================installing essential tools==================="
-sudo apt-get install vim openvpn wget curl xz-utils
+sudo apt-get install vim openvpn wget curl xz-utils -y
 
 echo "===================installing nodejs==================="
 wget https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz
