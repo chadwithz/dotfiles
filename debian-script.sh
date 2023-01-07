407412df5acbce710e5c79e30ef2f8c73cb2295b
name's already taken <93415755+chadwithz@users.noreply.github.com>
2023-01-07 21:45:12 +0700

    Update debian-script.sh

--- debian-script.sh
+++ debian-script.sh
@@ -7,7 +7,7 @@ echo "===================installing essential tools==================="
 sudo apt-get install vim openvpn wget curl xz-utils -y
 
 echo "===================installing nodejs==================="
-wget https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v18.13.0/node-v18.13.0-linux-x64.tar.xz
 
 echo "===================unzipping nodejs==================="
 tar -xvf node-v16.15.0-linux-x64.tar.xz
@@ -32,6 +32,13 @@ sudo docker ps
 
 echo "===================Installing dependencies for puppeteer==================="
 sudo apt-get install ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils -y
+
+echo "====================================Docker Post Installation======================================="
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo "========================Verify that you can run docker commands without sudo====================="
+docker ps
 
 
 
